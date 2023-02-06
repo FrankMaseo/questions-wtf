5eab85a873672de930c01d77326fd0b178f73c65
Frank Maseo <leodubois@live.fr>
2023-02-06 19:12:40 +0100

    connected backend first stage - read

--- components/QuestionCard.js
+++ components/QuestionCard.js
@@ -1,93 +1,127 @@
-import React, { useState } from 'react';
-
-
-const questions = {
-    question: <>Would you rather give 1 million euros to 1 person or 1000&euro; to 1000 people?</>,
-    answers: [
-        {
-            key: 0,
-            value: <>1 million &euro; to a single person</>,
-            answerCount: 23
-        },
-        {
-            key: 1,
-            value: <>1000&euro; to 1000 people</>,
-            answerCount: 38
-        }
-    ],
-    results: [
-        {
-            key: 0,
-            value: 23
-        },
-        {
-            key: 1,
-            value: 38
-        }
-    ]
-}
-
+import { ref, onValue } from "@firebase/database";
+import { database } from '../config/firebase';
+import React, { useState, useContext, createContext, useEffect } from 'react';
 
 
 const QuestionCard = () => {
 
+    const [question, setQuestion] = useState({});
+    const [questionCount, setQuestionCount] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [questionId, setQuestionId] = useState(0);
+    const [displayResults, setDisplayResults] = useState(false);
+
+    useEffect(() => {
+        
+
+        let questionRef = ref(database, 'questions/' + questionId);
+
+        onValue(questionRef, (snapshot) => {
+            setIsLoading(true);
+            setQuestion(snapshot.val());
+            setIsLoading(false);
+        });
+        
+        
+    }, [questionId])
+
+    useEffect(() => {
+
+
+        let questionRef = ref(database, 'questionCount');
+        console.log('questions/' + questionId);
+
+        onValue(questionRef, (snapshot) => {
+            setIsLoading(true);
+            setQuestionCount(snapshot.val());
+            setIsLoading(false);
+        });
+
+
+    },)
+
+
+    const displayQuestionResults = () => {
+        setDisplayResults(!displayResults)
+    }
+    const updateQuestionId = () => {
+        displayQuestionResults();
+        setQuestionId(Math.floor(Math.random() * Math.floor(questionCount)));
+    }
+
+
+    
     return (
         //<div className="question-container">
-            <div className="question-card">
-                <QuestionHeader questions={questions} />
-                <QuestionAnswer questions={questions} />
-                <NextQuestion questions={questions} />
-            </div>
-
+        <>
+            {isLoading ?
+                <div className = "question-card">loading</div> :
+                <div className="question-card">
+                    <QuestionHeader questions={question} />
+                    <QuestionAnswer questions={question} displayResults={displayResults} displayQuestionResults={displayQuestionResults} />
+                    <NextQuestion updateFunction={updateQuestionId} />
+                </div>
+            }
+        </>
         //</div>
     )
 }
 
 const QuestionHeader = (props) => {
+    console.log(props.questions.answers)
     return (
         <span className="question">{props.questions.question}</span>
     );
 }
 
 const QuestionAnswer = (props) => {
-    const [displayResults, setDisplayResults] = useState(false);
+    
 
     const submitAnswer = (e) => {
         e.preventDefault();
-        setDisplayResults(true);
+        props.displayQuestionResults();
         //add function coming from props to submit answer to backend
     }
 
     return (
         <>
             <div className="questions answer">
-                {props.questions.answers.map((value) => {
-                    return <button
-                        key={value.key}
-                        onClick={submitAnswer}
-                        disabled={displayResults}
-                    >{value.value}</button>
-                })}
+                {
+                    props.questions.answers.map((value) => {
+                        return (<button
+                            key={value}
+                            onClick={submitAnswer}
+                            disabled={props.displayResults}
+                        >{value.answer}</button>
+                        );
+                    })
+                }
 
             </div>
-            {
-                displayResults ?
-                    <ul className="results">
-                        {props.questions.answers.map((value) => {
-                            return <li key={value.key}>{value.value}: {value.answerCount}</li>
-                        })}
-                    </ul>
+            
+            <ul className = "results">
+                {props.displayResults ?
+                    props.questions.answers.map((value) => {
+                        return (<li
+                            key={value}
+                        >{value.answer} : {value.answerCount}</li>
+                        );
+                    })
                     : null
-            }
-
+                }
+            </ul>
 
         </>
     );
 }
-const NextQuestion = () => {
+const NextQuestion = (props) => {
+
+    const newQuestion = () => {
+        props.updateFunction()
+    }
     return (
         <div className="questions new">
-            <button>New question</button>
+            <button onClick={newQuestion }>New question</button>
         </div>
     );
 }
